32b23b7e99ec538c7a9fcd058681f1c764ee9a58
cnishino <cnishino19820915@icloud.com>
2022-05-10 08:31:25 +0000

    [add]内部クラスを使うケース、確認問題

--- exercise1.java
+++ exercise1.java
@@ -110,3 +110,37 @@ public class Constants {
     }
 
 }
+// この定数をつかうコードは以下のようになる
+System.out.println(Constants.Cat.NAME); // "猫"
+System.out.println(Constants.Cat.LEG_COUNT); // 4
+System.out.println(Constants.Octopus.NAME);  // "タコ"
+System.out.println(Constants.Octopus.LEG_COUNT);  // 8
+
+// クラス間の関連性を強調するケース
+// 猫クラス
+public class Cat {
+  // 猫の爪クラス
+  public class CatNail {
+  }
+}
+// タコクラス
+public class Tako {
+  // タコの吸盤クラス
+  public class TakoSucker {
+  }
+}
+
+// 確認問題
+public class Human {
+
+  public class HumanLeg {
+    public void kick() {
+      System.out.println("蹴る");
+    }
+  }
+}
+Human human = new Human();
+Human.HumanLeg leg = human.new HumanLeg();
+leg.kick();
+// 又は
+new Human().new HumanLeg().kick();
